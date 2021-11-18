17b740226312a17d8b22fc64ec292c9e9b12fe92
JBatsyus <ju.kalabina@gmail.com>
2021-11-18 14:38:47 +0200

    добавила шрифты, стилизация карточки

--- card_eng/src/App.js
+++ card_eng/src/App.js
@@ -1,13 +1,24 @@
 import "./App.css";
 import Header from "./components/header/index.jsx";
-import Main from "./components/main/index.jsx";
+// import Main from "./components/main/index.jsx";
 import Footer from "./components/footer/index.jsx";
+import Card from "./components/card/index.jsx";
+import { words } from "./components/card/cardDate";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Main></Main>
+      {/* <Main> */}
+      {words.map(word => (
+        <Card
+          key={word.id}
+          word={word.word}
+          transcription={word.transcription}
+          translation={word.russian}
+        />
+      ))}
+      {/* </Main> */}
       <Footer></Footer>
     </div>
   );
--- card_eng/src/components/card/index.jsx
+++ card_eng/src/components/card/index.jsx
@@ -1,5 +1,14 @@
-import './Card.scss'
+import "./Card.scss";
 
-const Card =()=>{
-    
-}
+const Card = props => {
+  const { word, transcription, translation } = props;
+  return (
+    <div className="card_body">
+      <div className="card_word">{word}</div>
+      <div className="card_transcription">{transcription}</div>
+      <div className="card_translation">{translation}</div>
+    </div>
+  );
+};
+
+export default Card;
