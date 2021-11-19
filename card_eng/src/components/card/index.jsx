7a4e788cf68df02b5e4b9c70d91135e711032aa6
JBatsyus <ju.kalabina@gmail.com>
2021-11-19 14:37:13 +0200

    добавила кнопку перевода для карточки

--- card_eng/src/components/card/index.jsx
+++ card_eng/src/components/card/index.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
+
 import "./Card.scss";
 
 const Card = props => {
+  const [pressed, setPressed] = useState(false);
+
   const { word, transcription, translation } = props;
+
+  const handleChange = () => {
+    setPressed(!pressed);
+  };
   return (
     <div className="card_body">
       <div className="card_word">{word}</div>
       <div className="card_transcription">{transcription}</div>
-      <div className="card_translation">{translation}</div>
+      {pressed ? (
+        <div className="card_translation">{translation}</div>
+      ) : (
+        <button className="card_answer_btn" onClick={handleChange}>
+          check
+        </button>
+      )}
     </div>
   );
 };
