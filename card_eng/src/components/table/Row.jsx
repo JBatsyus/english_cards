1c109b7da563eb238938ed1e9a01d31d7b1af0c1
JBatsyus <ju.kalabina@gmail.com>
2021-11-20 23:31:25 +0200

    добавление компонента row

---
+++ card_eng/src/components/table/Row.jsx
@@ -0,0 +1,57 @@
+import ButtonDelete from "../buttons/ButtonDelete";
+import ButtonEdit from "../buttons/ButtonEdit";
+import ButtonSave from "../buttons/ButtonSave";
+import { useState } from "react";
+
+export const Row = props => {
+  const [editMode, setEditMode] = useState(false);
+
+  const handleEditChange = isEdit => setEditMode(isEdit);
+
+  return (
+    <tr key={props.id}>
+      <td>
+        <input
+          className="input_editMode"
+          type="text"
+          value={props.word}
+          onChange={() => null}
+          disabled={!editMode}
+        />
+      </td>
+      <td>
+        <input
+          className="input_editMode"
+          type="text"
+          value={props.transcription}
+          onChange={() => null}
+          disabled={!editMode}
+        />
+      </td>
+      <td>
+        <input
+          className="input_editMode"
+          type="text"
+          value={props.russian}
+          onChange={() => null}
+          disabled={!editMode}
+        />
+      </td>
+      <td>
+        {!editMode ? (
+          <ButtonEdit
+            className="btn_editMode"
+            onClick={() => handleEditChange(true)}
+          />
+        ) : null}
+        {editMode ? (
+          <ButtonSave
+            className="btn_editMode"
+            onClick={() => handleEditChange(false)}
+          />
+        ) : null}
+        <ButtonDelete />
+      </td>
+    </tr>
+  );
+};
--- card_eng/src/components/table/TableWords.jsx
+++ card_eng/src/components/table/TableWords.jsx
@@ -1,75 +1,8 @@
-import ButtonDelete from "../buttons/ButtonDelete";
-import ButtonEdit from "../buttons/ButtonEdit";
-import ButtonSave from "../buttons/ButtonSave";
+import { Row } from "./Row";
 import { table_words } from "../table/tableDate";
-import { useState } from "react";
 
 const TableWords = () => {
-  const [editMode, setEditMode] = useState(false);
-
-  const handleEditChange = isEdit => setEditMode(isEdit);
-
-  return table_words.map(cell => (
-    <tr key={cell.id}>
-      <td>
-        {editMode ? (
-          <input
-            className="input_editMode"
-            type="text"
-            value={cell.word}
-            onChange={() => null}
-          />
-        ) : (
-          cell.word
-        )}
-      </td>
-      <td>
-        {editMode ? (
-          <input
-            className="input_editMode"
-            type="text"
-            value={cell.transcription}
-            onChange={() => null}
-          />
-        ) : (
-          cell.transcription
-        )}
-      </td>
-      <td>
-        {editMode ? (
-          <input
-            className="input_editMode"
-            type="text"
-            value={cell.russian}
-            onChange={() => null}
-          />
-        ) : (
-          cell.russian
-        )}
-      </td>
-      <td>
-        {/* <div className="buttons"> */}
-        {!editMode ? (
-          <ButtonEdit
-            className="btn_editMode"
-            onClick={() => handleEditChange(true)}
-          />
-        ) : null}
-        {editMode ? (
-          <ButtonSave
-            className="btn_editMode"
-            onClick={() => handleEditChange(false)}
-          />
-        ) : null}
-        <ButtonDelete />
-        {/* </div> */}
-      </td>
-    </tr>
-  ));
+  return table_words.map(cell => <Row key={cell.id} {...cell} />);
 };
 
 export default TableWords;
-
-// {
-//   editMode ? (<input value="butterfly" onChange={() => null} />) : ( <span>butterfly</span>);
-// }
