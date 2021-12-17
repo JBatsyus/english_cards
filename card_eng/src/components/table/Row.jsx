e2f3ddb45ae2c633dd94f984865ebed84e184245
JBatsyus <ju.kalabina@gmail.com>
2021-12-17 13:39:16 +0200

    валидация формы с хуком

--- card_eng/src/components/table/Row.jsx
+++ card_eng/src/components/table/Row.jsx
@@ -1,9 +1,18 @@
 import ButtonDelete from "../buttons/ButtonDelete";
 import ButtonEdit from "../buttons/ButtonEdit";
 import ButtonSave from "../buttons/ButtonSave";
+import { useForm } from "react-hook-form";
 import { useState } from "react";
 
 export const Row = props => {
+  // register — это функция, которую нужно подключить к каждому из полей ввода в качестве ссылки.
+  // Функция register будет принимать значение, которое пользователь ввел в каждое поле, и проверять его. register также передаст каждое значение в функцию, которая будет вызвана при отправке формы
+  const { register, errors } = useForm({
+    mode: "onBlur",
+  });
+  // необходимо дописать кнопку и обернуть ее, но как
+
+  // состояние, отражающее изменения внутри инпутов
   const [editMode, setEditMode] = useState(false);
 
   const handleEditChange = isEdit => setEditMode(isEdit);
@@ -14,28 +23,55 @@ export const Row = props => {
         <input
           className="input_editMode"
           type="text"
-          defaultValue={props.word}
+          name="word"
+          ref={register({
+            required: true,
+            pattern: /^[A-Za-z]+$/i,
+          })}
+          placeholder="word"
+          value={props.word}
           onChange={() => null}
           disabled={!editMode}
         />
+        {errors.word && (
+          <span className="error">Поле обязательно к заполнению.</span>
+        )}
       </td>
       <td>
         <input
           className="input_editMode"
           type="text"
-          defaultValue={props.transcription}
+          name="transcription"
+          ref={register({
+            required: true,
+            pattern: /^[A-Za-z]+$/i,
+          })}
+          placeholder="transcription"
+          value={props.transcription}
           onChange={() => null}
           disabled={!editMode}
         />
+        {errors.transcription && (
+          <span className="error">Поле обязательно к заполнению.</span>
+        )}
       </td>
       <td>
         <input
           className="input_editMode"
           type="text"
-          defaultValue={props.russian}
+          name="russian"
+          ref={register({
+            required: true,
+            pattern: /[а-яё]+/i,
+          })}
+          placeholder="russian"
+          value={props.russian}
           onChange={() => null}
           disabled={!editMode}
         />
+        {errors.russian && (
+          <span className="error">Поле обязательно к заполнению.</span>
+        )}
       </td>
       <td>
         {!editMode ? (
