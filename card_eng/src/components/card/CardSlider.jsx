ca1ce56eb6460852ae97a116e155ff9f51e28d25
JBatsyus <ju.kalabina@gmail.com>
2021-12-24 23:55:10 +0200

    добавила слова в карточку с api

--- card_eng/src/components/card/CardSlider.jsx
+++ card_eng/src/components/card/CardSlider.jsx
@@ -1,6 +1,8 @@
 import Card from "./index";
 import "./CardSlider.scss";
-import { words } from "./cardDate";
+// import { words } from "./cardDate";
+import { useContext } from "react";
+import { DataContext } from "../context/context";
 import { useState, useCallback } from "react";
 import ButtonBack from "../buttons/ButtonBack";
 import ButtonNext from "../buttons/ButtonNext";
@@ -8,18 +10,19 @@ import ButtonNext from "../buttons/ButtonNext";
 const CardSlider = () => {
   const [selectedCard, setSelectedCard] = useState(0);
   const [count, setCount] = useState(0);
+  const { data } = useContext(DataContext);
 
   const handleClickButtonNext = () => {
     // бесконечная карусель
-    const newIdx = (selectedCard + 1) % words.length;
-    if (newIdx < words.length) {
+    const newIdx = (selectedCard + 1) % data.length;
+    if (newIdx < data.length) {
       setSelectedCard(newIdx);
     }
   };
 
   const handleClickButtonBack = () => {
     // переключение карточек в обратном направлении
-    const oldIdx = (selectedCard - 1 + words.length) % words.length;
+    const oldIdx = (selectedCard - 1 + data.length) % data.length;
     if (oldIdx >= 0) {
       setSelectedCard(oldIdx);
     }
@@ -34,24 +37,18 @@ const CardSlider = () => {
         <br /> Congratulations!
       </div>
       <div className="Card">
-        <ButtonBack
-          onClick={handleClickButtonBack}
-          // disabled={selectedCard === 0}
-        />
+        <ButtonBack onClick={handleClickButtonBack} />
         <Card
-          key={words[selectedCard].id}
-          english={words[selectedCard].english}
-          transcription={words[selectedCard].transcription}
-          translation={words[selectedCard].russian}
+          key={data[selectedCard].id}
+          english={data[selectedCard].english}
+          transcription={data[selectedCard].transcription}
+          translation={data[selectedCard].russian}
           increment={increment}
         />
-        <ButtonNext
-          onClick={handleClickButtonNext}
-          // disabled={selectedCard === words.length - 1}
-        />
+        <ButtonNext onClick={handleClickButtonNext} />
       </div>
       <div className="cardCounter">
-        {selectedCard + 1} / {words.length}
+        {selectedCard + 1} / {data.length}
       </div>
     </div>
   );
--- card_eng/src/components/table/TableWords.jsx
+++ card_eng/src/components/table/TableWords.jsx
@@ -1,13 +1,9 @@
 import { Row } from "./Row";
-// import { table_words } from "../table/tableDate";
-
 import { useContext } from "react";
 import { DataContext } from "../context/context";
 
 const TableWords = () => {
   const { data } = useContext(DataContext);
-  // if (isLoading) return <Loader />;
-
   return data?.map(cell => <Row key={cell.id} {...cell} />);
 };
 
