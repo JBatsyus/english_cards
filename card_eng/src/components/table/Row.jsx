d73f601fa759f5f12614f028a21f115cf04c654d
JBatsyus <ju.kalabina@gmail.com>
2022-01-03 19:28:17 +0200

    изменила Row

--- card_eng/src/components/table/Row.jsx
+++ card_eng/src/components/table/Row.jsx
@@ -3,118 +3,130 @@ import ButtonEdit from "../buttons/ButtonEdit";
 import ButtonSave from "../buttons/ButtonSave";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
+import { observer, inject } from "mobx-react";
+import ErrorServer from "../error/errorServer";
+import Loader from "../loader/loader";
 
-export const Row = props => {
-  // register — это функция, которую нужно подключить к каждому из полей ввода в качестве ссылки.
-  // Функция register будет принимать значение, которое пользователь ввел в каждое поле, и проверять его. register также передаст каждое значение в функцию, которая будет вызвана при отправке формы
-  const {
-    register,
-    formState: { errors },
-  } = useForm({
-    mode: "onChange",
-  });
+export const Row = inject(["wordsStore"])(
+  observer(({ wordsStore }) => {
+    const allWords = wordsStore.words;
+    const errorMob = wordsStore.error;
+    const loaderMob = wordsStore.isLoading;
+    // register — это функция, которую нужно подключить к каждому из полей ввода в качестве ссылки.
+    // Функция register будет принимать значение, которое пользователь ввел в каждое поле, и проверять его. register также передаст каждое значение в функцию, которая будет вызвана при отправке формы
+    const {
+      register,
+      formState: { errors },
+    } = useForm({
+      mode: "onChange",
+    });
 
-  // состояние, отражающее изменения внутри инпутов
-  const [editMode, setEditMode] = useState(false);
-  const [data, setData] = useState({
-    english: props.english,
-    transcription: props.transcription,
-    russian: props.russian,
-  });
-  const notValidWords =
-    data.english === "" || data.transcription === "" || data.russian === "";
+    // состояние, отражающее изменения внутри инпутов
+    const [editMode, setEditMode] = useState(false);
+    const [data, setData] = useState({
+      english: allWords.english,
+      transcription: allWords.transcription,
+      russian: allWords.russian,
+    });
+    const notValidWords =
+      data.english === "" || data.transcription === "" || data.russian === "";
 
-  const handleEditChange = isEdit => {
-    if (!notValidWords) {
-      console.log(data);
-    }
-    setEditMode(isEdit);
-  };
-  const handleChange = event =>
-    setData({ ...data, [event.target.name]: event.target.value });
+    const handleEditChange = isEdit => {
+      if (!notValidWords) {
+        console.log(data);
+      }
+      setEditMode(isEdit);
+    };
+    const handleChange = event =>
+      setData({ ...data, [event.target.name]: event.target.value });
 
-  return (
-    <tr key={props.id}>
-      <td>
-        <input
-          className={`input_editMode ${
-            !data.english.length ? "inputError" : ""
-          }`}
-          type="text"
-          name="english"
-          value={data.english}
-          disabled={!editMode}
-          {...register("english", {
-            required: true,
-            pattern: /^[A-Za-z]+$/i,
-            onChange: handleChange,
-          })}
-        />
+    if (errorMob) return <ErrorServer />;
+    if (loaderMob) return <Loader />;
 
-        {
+    return (
+      <tr key={allWords.id}>
+        <td>
+          <input
+            className={`input_editMode ${
+              !data.english.length ? "inputError" : ""
+            }`}
+            type="text"
+            name="english"
+            value={data.english}
+            disabled={!editMode}
+            {...register("english", {
+              required: true,
+              pattern: /^[A-Za-z]+$/i,
+              onChange: handleChange,
+            })}
+          />
+
+          {
+            <div className="error">
+              {errors.english?.type === "required" &&
+                "Заполните, пожалуйста, поле"}
+              {errors.english?.type === "pattern" &&
+                "Только буквы латинского алфавита"}
+            </div>
+          }
+        </td>
+        <td>
+          <input
+            className={`input_editMode ${
+              !data.transcription.length ? "inputError" : ""
+            }`}
+            type="text"
+            name="transcription"
+            value={data.transcription}
+            disabled={!editMode}
+            {...register("transcription", {
+              required: true,
+              onChange: handleChange,
+            })}
+          />
           <div className="error">
-            {errors.english?.type === "required" &&
+            {errors.transcription?.type === "required" &&
               "Заполните, пожалуйста, поле"}
-            {errors.english?.type === "pattern" &&
-              "Только буквы латинского алфавита"}
           </div>
-        }
-      </td>
-      <td>
-        <input
-          className={`input_editMode ${
-            !data.transcription.length ? "inputError" : ""
-          }`}
-          type="text"
-          name="transcription"
-          value={data.transcription}
-          disabled={!editMode}
-          {...register("transcription", {
-            required: true,
-            onChange: handleChange,
-          })}
-        />
-        <div className="error">
-          {errors.transcription?.type === "required" &&
-            "Заполните, пожалуйста, поле"}
-        </div>
-      </td>
-      <td>
-        <input
-          className={`input_editMode ${
-            !data.russian.length ? "inputError" : ""
-          }`}
-          type="text"
-          name="russian"
-          value={data.russian}
-          disabled={!editMode}
-          {...register("russian", {
-            required: true,
-            pattern: /^[\u0400-\u04FF]+$/i,
-            onChange: handleChange,
-          })}
-        />
-        <div className="error">
-          {errors.russian?.type === "required" && "Заполните, пожалуйста, поле"}
-          {errors.russian?.type === "pattern" &&
-            "Только буквы русского алфавита"}
-        </div>
-      </td>
-      <td>
-        {!editMode ? (
-          <ButtonEdit
-            className="btn_editMode"
-            onClick={() => handleEditChange(true)}
+        </td>
+        <td>
+          <input
+            className={`input_editMode ${
+              !data.russian.length ? "inputError" : ""
+            }`}
+            type="text"
+            name="russian"
+            value={data.russian}
+            disabled={!editMode}
+            {...register("russian", {
+              required: true,
+              pattern: /^[\u0400-\u04FF]+$/i,
+              onChange: handleChange,
+            })}
           />
-        ) : (
-          <ButtonSave
-            className="btn_editMode"
-            onClick={() => handleEditChange(false)}
-            disabled={Object.keys(errors).length}
-          />
-        )}
-        <ButtonDelete />
-      </td>
-    </tr>
-  );
-};
+          <div className="error">
+            {errors.russian?.type === "required" &&
+              "Заполните, пожалуйста, поле"}
+            {errors.russian?.type === "pattern" &&
+              "Только буквы русского алфавита"}
+          </div>
+        </td>
+        <td>
+          {!editMode ? (
+            <ButtonEdit
+              className="btn_editMode"
+              onClick={() => handleEditChange(true)}
+            />
+          ) : (
+            <ButtonSave
+              className="btn_editMode"
+              onClick={() => handleEditChange(false)}
+              disabled={Object.keys(errors).length}
+            />
+          )}
+          <ButtonDelete />
+        </td>
+      </tr>
+    );
+  }),
+);
